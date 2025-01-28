88338bcb2db26cf96993f24b53bf0fb3e0fef751
Anatoly <anatoly.troinov@yandex.ru>
2025-01-28 23:02:29 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+class Task:
+    def __init__(self, name, date, priority):
+        self.__name = name
+        self.__date = date
+        self.__priority = priority
+
+class TaskManager:
+    def __init__(self):
+        pass
+
+class Priority:
+    def __init__(self, level):
+        self.level = level
+
+class TaskGUI:
+    def __init__(self, master):
+        self.__master = master
+
+class ThemeChange:
+    def __init__(self):
+        pass
